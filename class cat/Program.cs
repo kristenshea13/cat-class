13a9042b73a39bf77fb3b3071eeae119d84b0637
Kristen Shea <kstar61@live.com>
2018-09-03 22:03:11 -0500

    created a "cat" class, used method to determine adoption fee, used string formatting to have adoption fee print to console as currency

--- class cat/Program.cs
+++ class cat/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 
-namespace class_cat
+namespace class_Cat
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create new cat from Cat class
+            Cat myCat = new Cat();
+            {
+                myCat.FurColor = "Black";
+                myCat.BodySize = "Medium";
+                myCat.FurLength = "Short";
+                myCat.Age = 4;
+            };
+            
+            //print properties to console in descriptive sentence.
+            Console.WriteLine($"My cat is a {myCat.BodySize}-sized cat, with {myCat.FurColor}-colored fur. " +
+                $"My cat's fur is {myCat.FurLength}. My cat is {myCat.Age} years old.");
+
+            //format return from method below as currency
+            string adoptionfee = $"{AdoptPrice(myCat.FurLength, myCat.Age):C}";
+            
+            //print adoption fee as currency to console
+            Console.WriteLine("I adopted him for {0}", adoptionfee);
+
+
+
+
+            Console.ReadLine();
         }
+
+        private static decimal AdoptPrice(string FurLength, int Age)
+        {
+            //declare fee as decimal type
+            decimal fee;
+
+            //if/else if/else statements to determine adoption fee of cat based on two variables
+            if (FurLength == "Long" && Age <= 5)
+                fee = 125;
+            else if (FurLength == "Short" && Age <= 5)
+                fee = 100;
+            else if (FurLength == "Medium" && Age <= 5)
+                fee = 75.25M;
+            else
+                fee = 55.50M;
+
+            //return specified adoption fee
+            return fee;
+
+        }
+
+
     }
+    
+
 }
---
+++ class cat/cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_Cat
+{
+    public class Cat
+    {
+        public string FurColor { get; set; }
+        public string BodySize { get; set; }
+        public string FurLength { get; set; }
+        public int Age { get; set; }
+
+
+
+        
+
+        //public decimal DetermineMarketValue()
+        //{
+        //    decimal carValue;
+
+        //    if (Year > 1990)
+        //        carValue = 10000;
+        //    else
+        //        carValue = 2000;
+        //    return carValue;
+
+        //}
+    }
+}
